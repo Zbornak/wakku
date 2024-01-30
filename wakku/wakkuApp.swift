d2f135e2a71c243b8778a96bbe8414ae0d2404eb
zbornak <zbornak@protonmail.com>
2024-01-30 10:09:25 +0000

    Initial Commit

---
+++ wakku/wakkuApp.swift
@@ -0,0 +1,17 @@
+//
+//  wakkuApp.swift
+//  wakku
+//
+//  Created by Mark Strijdom on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct wakkuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
